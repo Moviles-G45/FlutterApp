04da38507c538ea63bef8b45a8822755a6e2dc60
Jcastilloq1 <j.castilloq@uniandes.edu.co>
2025-03-18 10:19:59 -0500

    c

--- flutter/lib/theme/app_theme.dart
+++ flutter/lib/theme/app_theme.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-const Color _customColor = Color(0xFF5C11D4);
+const Color _customColor = Color(0xFFF1FFF3);
 
 const List<Color> _colorThemes = [
   _customColor,
-  Colors.blue,
-  Colors.teal,
-  Colors.green,
-  Colors.yellow,
-  Colors.orange,
-  Colors.pink,
+  Color(0xFF18EF09),
+  Color(0xFF067DC3),
+  Color(0xFF052224),
+  Color(0xFFB7D9EE),
+  Color(0xFF6DB6FE),
+  Color(0xFF00D09E),
+  Color(0xFF0068FF),
+  Color(0xFF3299FF),
+  Color(0xFF0E3E3E),
+  Color(0xFF4F97B5),
 
 ];
 
