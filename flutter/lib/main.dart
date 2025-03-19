110f17d2cba947de2d6e29168ee85dde3ade9db1
Miguel Ariza <m.arizaj@uniandes.edu.co>
2025-03-18 21:05:38 -0500

    Cambio

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:finances/theme/app_theme.dart';
+import 'config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'features/home/screens/home.dart';
 import 'screens/launch_screen.dart';
