d13beaa910eed508e8c3f7c7dbfa3b7f585af8cb
Jcastilloq1 <j.castilloq@uniandes.edu.co>
2025-03-19 16:22:28 -0500

    ca

--- flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-
-import geolocator_apple
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-
 import firebase_auth
 import firebase_core
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
