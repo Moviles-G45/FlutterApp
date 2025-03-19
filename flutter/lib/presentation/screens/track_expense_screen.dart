65ff546c0a2ee9bc62c9393ead7b0a53e9078f3d
Jcastilloq1 <j.castilloq@uniandes.edu.co>
2025-03-19 01:03:26 -0500

    CambioTrack

--- flutter/lib/presentation/screens/track_expense_screen.dart
+++ flutter/lib/presentation/screens/track_expense_screen.dart
@@ -1,3 +1,4 @@
+import 'package:finances/config/theme/colors.dart';
 import 'package:finances/presentation/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import '../widgets/expense_widgets.dart';
@@ -10,26 +11,36 @@ class TrackExpenseScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Add Expenses", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.blue.shade700,
+        backgroundColor: AppColors.background,
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          icon: const Icon(Icons.arrow_back, color: AppColors.cardBackground),
           onPressed: () => Navigator.pop(context),
         ),
         actions: [
-          IconButton(icon: const Icon(Icons.notifications, color: Colors.white), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.notifications, color: AppColors.cardBackground), onPressed: () {}),
         ],
       ),
-      body: Padding(
+      body: Container(
+        decoration: BoxDecoration(
+                  color: AppColors.cardBackground,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+        ),
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const SizedBox(height: 30),
             ExpenseLabel(text: "Date"),
-            ExpenseInputField(placeholder: "April 30, 2024", icon: Icons.calendar_today),
+            ExpenseDatePicker(hintText: "Select a date"),
+            // ExpenseInputField(placeholder: "April 30, 2024", icon: Icons.calendar_today),
             const SizedBox(height: 10),
             ExpenseLabel(text: "Category"),
-            ExpenseInputField(placeholder: "Select the category", icon: Icons.arrow_drop_down),
+            CategoiesInputField(placeholder: 'Select the category', 
+            categoriasList: ["Food", "Transport", "Entertainment", "Shopping", "Other"]),
             const SizedBox(height: 10),
             ExpenseLabel(text: "Amount"),
             ExpenseInputField(placeholder: "\$26.00"),
