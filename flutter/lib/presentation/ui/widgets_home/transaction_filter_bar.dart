ee831582428f40dbba5e8bee4e43ecfbc4b29ef4
Gabriela <g.garcias2@uniandes.edu.co>
2025-05-15 22:10:34 -0500

    Agregar métodos para manejar presupuestos y transacciones, y mejorar la verificación de conexión en la interfaz de usuario.

--- flutter/lib/presentation/ui/widgets_home/transaction_filter_bar.dart
+++ flutter/lib/presentation/ui/widgets_home/transaction_filter_bar.dart
@@ -1,3 +1,4 @@
+import 'package:finances/presentation/viewmodels/home_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -13,6 +14,18 @@ class TransactionFilterBar extends StatefulWidget {
 class _TransactionFilterBarState extends State<TransactionFilterBar> {
   DateTime? _startDate;
   DateTime? _endDate;
+  late final HomeViewModel homeVM;
+  late final bool isOffline;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      homeVM = Provider.of<HomeViewModel>(context, listen: false);
+      isOffline = homeVM.isOffline;
+    });
+  }
+
 
   Future<void> _selectDate(BuildContext context, bool isStart) async {
     DateTime initialDate = DateTime.now();
@@ -70,6 +83,12 @@ void _applyFilter() {
   final viewModel = Provider.of<TransactionViewModel>(context, listen: false);
   final now = DateTime.now();
 
+  // Verificar si hay conexión a internet antes de aplicar el filtro
+  if (isOffline) {
+    _showMessage(context, "You are offline. Please check your connection.", Colors.redAccent);
+    return;
+  }
+
   try {
     final start = _startDate ?? DateTime(now.year, now.month, 1);
     final end = _endDate ?? DateTime(now.year, now.month + 1, 0);
@@ -82,13 +101,16 @@ void _applyFilter() {
     print("Applying date filter: From $start to $end");
     viewModel.setDateRange(start, end);
     _showMessage(context, "Filter applied successfully.", Colors.green);
-     _startDate = null;
-              _endDate = null;
+
+    // Limpiar las fechas después de aplicar el filtro
+    _startDate = null;
+    _endDate = null;
   } catch (e) {
     _showMessage(context, "Error applying filter. Please try again.", Colors.redAccent);
     print("Error applying filter: $e");
   }
 }
+
 
 
 
